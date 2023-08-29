7f5a956189c5c7a0ad0cf023ecf4d22634d6a90c
viniciuscasarin <casarinvinicius@gmail.com>
2023-08-28 21:46:48 -0300

    flappy bird: adding sprite to a scene

--- flappy-bird/src/index.js
+++ flappy-bird/src/index.js
@@ -21,8 +21,12 @@ new Phaser.Game(config);
 
 function preload () {
   this.load.image('sky', 'assets/sky.png');
+  this.load.image('bird', 'assets/bird.png');
 }
+
+let bird = null
 
 function create () {
   this.add.image(0, 0, 'sky').setOrigin(0, 0);
+  bird = this.add.sprite(config.width * 0.1, config.height / 2, 'bird').setOrigin(0, 0);
 }
