3e46799706184812d9a091f6c8e5c8b6db5a6175
viniciuscasarin <casarinvinicius@gmail.com>
2023-08-29 22:43:43 -0300

    flappy bird: basic pipes

--- flappy-bird/src/index.js
+++ flappy-bird/src/index.js
@@ -1,17 +1,12 @@
 
 import Phaser from "phaser";
 
-const VELOCITY = 400
-
 const config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
   physics: {
     default: 'arcade',
-    arcade: {
-      gravity: { y: VELOCITY }
-    }
   },
   scene: {
     preload,
@@ -20,6 +15,7 @@ const config = {
   }
 };
 
+const VELOCITY = 400
 const FLAP_VELOCITY = 250
 const INITIAL_BIRD_X = config.width * 0.1
 const INITIAL_BIRD_Y = config.height / 2
@@ -29,13 +25,21 @@ new Phaser.Game(config);
 function preload () {
   this.load.image('sky', 'assets/sky.png');
   this.load.image('bird', 'assets/bird.png');
+  this.load.image('pipe', 'assets/pipe.png');
 }
 
 let bird = null
+let upperPipe = null
+let lowerPipe = null
 
 function create () {
   this.add.image(0, 0, 'sky').setOrigin(0, 0);
+  
   bird = this.physics.add.sprite(INITIAL_BIRD_X, INITIAL_BIRD_Y, 'bird').setOrigin(0, 0);
+  bird.body.gravity.y = VELOCITY;
+
+  upperPipe = this.physics.add.sprite(500, 100, 'pipe').setOrigin(0, 1);
+  lowerPipe = this.physics.add.sprite(500, upperPipe.y + 100, 'pipe').setOrigin(0, 0);
 
   this.input.on('pointerdown', flap)
   this.input.keyboard.on('keydown_SPACE', flap)
